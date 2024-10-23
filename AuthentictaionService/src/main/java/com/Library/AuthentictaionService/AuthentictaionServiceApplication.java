e25085e4c12d20bd84b7080c9c8c6489ab3eb6f4
CHETHAN-DORESWAMY <chethandoreswamy22@gmail.com>
2024-10-23 15:33:59 +0530

    fix: added discovery client

---
+++ AuthentictaionService/src/main/java/com/Library/AuthentictaionService/AuthentictaionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Library.AuthentictaionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentictaionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentictaionServiceApplication.class, args);
+	}
+
+}
---
+++ AuthentictaionService/src/test/java/com/Library/AuthentictaionService/AuthentictaionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Library.AuthentictaionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentictaionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
